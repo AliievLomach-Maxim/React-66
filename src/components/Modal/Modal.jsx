4c2b78ffd8dee7b9bc87d6f98ba38c58d75f79d8
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-03-22 22:11:12 +0200

    add less

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,83 @@
-import { Component } from 'react'
+import { useEffect } from 'react'
 
-class Modal extends Component {
-	state = {}
+const Modal = ({ closeModal, children }) => {
+	useEffect(() => {
+		const handlePressESC = (e) => {
+			console.log('object :>> ', Date.now())
+			if (e.code === 'Escape') closeModal()
+		}
 
-	componentDidMount() {
-		window.addEventListener('keydown', this.handlePressESC)
-	}
+		window.addEventListener('keydown', handlePressESC)
+		return () => {
+			window.removeEventListener('keydown', handlePressESC)
+		}
+	}, [closeModal])
 
-	componentWillUnmount() {
-		window.removeEventListener('keydown', this.handlePressESC)
-	}
-
-	handlePressESC = (e) => {
-		console.log('object :>> ', Date.now())
-		if (e.code === 'Escape') this.props.closeModal()
-	}
-
-	render() {
-		const { closeModal, children } = this.props
-		return (
-			<div
-				className='modal fade show'
-				style={{ display: 'block', backdropFilter: 'blur(5px)' }}
-			>
-				<div className='modal-dialog'>
-					<div className='modal-content'>
-						<div className='modal-header'>
-							<h5 className='modal-title'> Modal</h5>
-							<button
-								type='button'
-								className='btn-close'
-								aria-label='Close'
-								onClick={closeModal}
-							></button>
-						</div>
-						<div className='modal-body'>{children}</div>
+	return (
+		<div
+			className='modal fade show'
+			style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+		>
+			<div className='modal-dialog'>
+				<div className='modal-content'>
+					<div className='modal-header'>
+						<h5 className='modal-title'> Modal</h5>
+						<button
+							type='button'
+							className='btn-close'
+							aria-label='Close'
+							onClick={closeModal}
+						></button>
 					</div>
+					<div className='modal-body'>{children}</div>
 				</div>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default Modal
+
+// class Modal extends Component {
+// 	state = {}
+
+// 	componentDidMount() {
+// 		window.addEventListener('keydown', this.handlePressESC)
+// 	}
+
+// 	componentWillUnmount() {
+// 		window.removeEventListener('keydown', this.handlePressESC)
+// 	}
+
+// 	handlePressESC = (e) => {
+// 		console.log('object :>> ', Date.now())
+// 		if (e.code === 'Escape') this.props.closeModal()
+// 	}
+
+// 	render() {
+// 		const { closeModal, children } = this.props
+// 		return (
+// 			<div
+// 				className='modal fade show'
+// 				style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+// 			>
+// 				<div className='modal-dialog'>
+// 					<div className='modal-content'>
+// 						<div className='modal-header'>
+// 							<h5 className='modal-title'> Modal</h5>
+// 							<button
+// 								type='button'
+// 								className='btn-close'
+// 								aria-label='Close'
+// 								onClick={closeModal}
+// 							></button>
+// 						</div>
+// 						<div className='modal-body'>{children}</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default Modal
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,108 +1,174 @@
-import React, { Component } from 'react'
+import { useEffect, useState } from 'react'
+
+import toast from 'react-hot-toast'
 
 import ToDo from '../ToDo/ToDo'
 import FormToDo from '../FormToDo/FormToDo'
 
-import todo from '../../todo.json'
 import { nanoid } from 'nanoid'
 
-class ToDoList extends Component {
-	state = {
-		todoList: '',
-		isDelete: false,
-		isCreate: false,
-	}
+const ToDoList = () => {
+	const [todoList, setTodoList] = useState('')
 
-	componentDidMount() {
-		// localStorage.setItem('todo', JSON.stringify(todo))
-		if (localStorage.getItem('todo'))
-			this.setState({
-				todoList: JSON.parse(localStorage.getItem('todo')),
-			})
-	}
+	useEffect(() => {
+		const localTodo = localStorage.getItem('todo')
+		if (localTodo) setTodoList(JSON.parse(localTodo))
+	}, [])
 
-	componentDidUpdate(_, prevState) {
-		if (prevState.todoList.length > this.state.todoList.length) {
-			localStorage.setItem('todo', JSON.stringify(this.state.todoList))
-			this.setState({
-				isDelete: true,
-				// todo: localStorage.getItem('todo'),
-			})
-			setTimeout(() => {
-				this.setState({ isDelete: false })
-			}, 1500)
-		}
-		if (prevState.todoList.length < this.state.todoList.length) {
-			localStorage.setItem('todo', JSON.stringify(this.state.todoList))
-			this.setState({
-				isCreate: true,
-				// todo: localStorage.getItem('todo'),
-			})
-			setTimeout(() => {
-				this.setState({ isCreate: false })
-			}, 1500)
-		}
-	}
+	useEffect(() => {
+		todoList && localStorage.setItem('todo', JSON.stringify(todoList))
+	}, [todoList])
 
-	handleCheckCompleted = (id) => {
-		this.setState((prevState) => ({
-			todoList: prevState.todoList.map((todo) =>
+	const handleCheckCompleted = (id) => {
+		setTodoList((prevTodoList) => {
+			return prevTodoList.map((todo) =>
 				todo.id === id ? { ...todo, completed: !todo.completed } : todo
-			),
-		}))
-	}
-
-	handleDelete = (id) => {
-		this.setState((prev) => ({
-			todoList: prev.todoList.filter((todo) => todo.id !== id),
-		}))
-	}
-
-	addToDo = (value) => {
-		this.setState((prev) => {
-			return {
-				todoList: [
-					...prev.todoList,
-					{
-						id: nanoid(),
-						title: value,
-						completed: false,
-					},
-				],
-			}
+			)
 		})
 	}
 
-	render() {
-		return (
-			<>
-				<h1>My To-Do list</h1>
-				{this.state.isDelete && (
-					<div className='alert alert-danger' role='alert'>
-						To-do delete successfully!
-					</div>
-				)}
-				{this.state.isCreate && (
-					<div className='alert alert-success' role='alert'>
-						Create to-do successfully!
-					</div>
-				)}
-				<FormToDo addToDo={this.addToDo} />
-				{this.state.todoList && (
-					<ul className='list-group list-group-flush'>
-						{this.state.todoList.map((todo) => (
-							<ToDo
-								key={todo.id}
-								todo={todo}
-								handleCheckCompleted={this.handleCheckCompleted}
-								handleDelete={this.handleDelete}
-							/>
-						))}
-					</ul>
-				)}
-			</>
-		)
+	const handleDelete = (id) => {
+		setTodoList((prevTodoList) => {
+			return prevTodoList.filter((todo) => todo.id !== id)
+		})
+
+		toast.error('Delete successfully')
 	}
+
+	const addToDo = (value) => {
+		setTodoList((prevTodoList) => {
+			return [
+				...prevTodoList,
+				{
+					id: nanoid(),
+					title: value,
+					completed: false,
+				},
+			]
+		})
+
+		toast.success('Create successfully')
+	}
+
+	return (
+		<>
+			<h1>My To-Do list</h1>
+			<FormToDo addToDo={addToDo} />
+			{todoList && (
+				<ul className='list-group list-group-flush'>
+					{todoList.map((todo) => (
+						<ToDo
+							key={todo.id}
+							todo={todo}
+							handleCheckCompleted={handleCheckCompleted}
+							handleDelete={handleDelete}
+						/>
+					))}
+				</ul>
+			)}
+		</>
+	)
 }
 
 export default ToDoList
+
+// class ToDoList extends Component {
+// 	state = {
+// 		todoList: '',
+// 		isDelete: false,
+// 		isCreate: false,
+// 	}
+
+// 	componentDidMount() {
+// 		// localStorage.setItem('todo', JSON.stringify(todo))
+// 		if (localStorage.getItem('todo'))
+// 			this.setState({
+// 				todoList: JSON.parse(localStorage.getItem('todo')),
+// 			})
+// 	}
+
+// 	componentDidUpdate(_, prevState) {
+// 		if (prevState.todoList.length > this.state.todoList.length) {
+// 			localStorage.setItem('todo', JSON.stringify(this.state.todoList))
+// 			this.setState({
+// 				isDelete: true,
+// 				// todo: localStorage.getItem('todo'),
+// 			})
+// 			setTimeout(() => {
+// 				this.setState({ isDelete: false })
+// 			}, 1500)
+// 		}
+// 		if (prevState.todoList.length < this.state.todoList.length) {
+// 			localStorage.setItem('todo', JSON.stringify(this.state.todoList))
+// 			this.setState({
+// 				isCreate: true,
+// 				// todo: localStorage.getItem('todo'),
+// 			})
+// 			setTimeout(() => {
+// 				this.setState({ isCreate: false })
+// 			}, 1500)
+// 		}
+// 	}
+
+// 	handleCheckCompleted = (id) => {
+// 		this.setState((prevState) => ({
+// 			todoList: prevState.todoList.map((todo) =>
+// 				todo.id === id ? { ...todo, completed: !todo.completed } : todo
+// 			),
+// 		}))
+// 	}
+
+// 	handleDelete = (id) => {
+// 		this.setState((prev) => ({
+// 			todoList: prev.todoList.filter((todo) => todo.id !== id),
+// 		}))
+// 	}
+
+// 	addToDo = (value) => {
+// 		this.setState((prev) => {
+// 			return {
+// 				todoList: [
+// 					...prev.todoList,
+// 					{
+// 						id: nanoid(),
+// 						title: value,
+// 						completed: false,
+// 					},
+// 				],
+// 			}
+// 		})
+// 	}
+
+// 	render() {
+// 		return (
+// 			<>
+// 				<h1>My To-Do list</h1>
+// 				{this.state.isDelete && (
+// 					<div className='alert alert-danger' role='alert'>
+// 						To-do delete successfully!
+// 					</div>
+// 				)}
+// 				{this.state.isCreate && (
+// 					<div className='alert alert-success' role='alert'>
+// 						Create to-do successfully!
+// 					</div>
+// 				)}
+// 				<FormToDo addToDo={this.addToDo} />
+// 				{this.state.todoList && (
+// 					<ul className='list-group list-group-flush'>
+// 						{this.state.todoList.map((todo) => (
+// 							<ToDo
+// 								key={todo.id}
+// 								todo={todo}
+// 								handleCheckCompleted={this.handleCheckCompleted}
+// 								handleDelete={this.handleDelete}
+// 							/>
+// 						))}
+// 					</ul>
+// 				)}
+// 			</>
+// 		)
+// 	}
+// }
+
+// export default ToDoList
